f118c633b007e61520dafe4a94c9778b9e06fe14
west <781967140@qq.com>
2019-01-29 19:33:55 +0800

    add chat test and index.html

--- test/chat.spec.js
+++ test/chat.spec.js
@@ -1,11 +1,22 @@
 const request = require('supertest');
 const { app } = require('../server');
+const { expect } = require('chai');
 
 describe('server', function() {
 	it('should serve html when a get request is made', function(done) {
 		request(app)
 		.get('/')
-		.expect('Content-Type', 'text/html; charset-UTF-8')
+		.expect('Content-Type', 'text/html; charset=UTF-8')
 		.expect(200, done);
 	});
+});
+
+it('should serve the correct html page', function(done) {
+	request(app)
+	.get('/')
+	.end((error, response) => {
+		expect(response.text.match(/<title>Cold Brew Tutorial<\/title>/))
+		.to.not.be.null;
+		done();
+	})
 });
