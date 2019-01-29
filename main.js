29c4f6397f3ea13d389c25238ec2cc1c317976f8
west <781967140@qq.com>
2019-01-29 20:04:27 +0800

    move test into server and add new test for client about submit text send request

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+	$('form').on('submit', (e) => {
+		e.preventDefault();
+	});
+});
--- test/chat.spec.js
+++ test/chat.spec.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const { app } = require('../server');
 const { expect } = require('chai');
+const coldBrew = require('cold-brew');
+const { Key, By, until } = require('selenium-webdriver');
 
 describe('server', function() {
 	it('should serve html when a get request is made', function(done) {
@@ -9,14 +11,49 @@ describe('server', function() {
 		.expect('Content-Type', 'text/html; charset=UTF-8')
 		.expect(200, done);
 	});
+
+	it('should serve the correct html page', function(done) {
+		request(app)
+		.get('/')
+		.end((error, response) => {
+			expect(response.text.match(/<title>Cold Brew Tutorial<\/title>/))
+			.to.not.be.null;
+			done();
+		})
+	});
 });
 
-it('should serve the correct html page', function(done) {
-	request(app)
-	.get('/')
-	.end((error, response) => {
-		expect(response.text.match(/<title>Cold Brew Tutorial<\/title>/))
-		.to.not.be.null;
-		done();
+
+
+const PORT = 3000;
+const ADDRESS = 'http://localhost:' + PORT + '/';
+
+describe('client-side messenger application', function() {
+	let client;
+
+	beforeEach(function() {
+		client = coldBrew.createClient();
+	});
+
+	it('should not reload the page when the text form is submitted', function(done) {
+		this.timeout(5000);
+
+		client.get(ADDRESS);
+
+		client.do([
+			['sendKeys', 'form input', {type: 'text'}, 'hello word' + Key.ENTER],
+			['click', 'form button', {}]
+		]);
+
+		client.executeScript(function() {
+			return window.location.href;
+		}).then((url) => {
+			expect(url).to.equal(ADDRESS);
+			done();
+		}) ;
+
+		afterEach(function(done) {
+			client.quit().then(() => done());
+		});
 	})
 });
